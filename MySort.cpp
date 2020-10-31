4896615fea3e115f6aa807450042d73c09cd4475
Bestin Lalu <46861674+gazarack@users.noreply.github.com>
2020-10-31 14:24:44 +0530

    Update MySort.cpp

--- MySort.cpp
+++ MySort.cpp
@@ -135,7 +135,24 @@ void merger_sort(int arr[], int arr_size){
 }
 #endif
 
-
+void swap(int *xp, int *yp)  
+{  
+    int temp = *xp;  
+    *xp = *yp;  
+    *yp = temp;  
+}  
+  
+// A function to implement bubble sort  
+void bubbleSort(int arr[], int n)  
+{  
+    int i, j;  
+    for (i = 0; i < n-1; i++)      
+      
+    // Last i elements are already in place  
+    for (j = 0; j < n-i-1; j++)  
+        if (arr[j] > arr[j+1])  
+            swap(&arr[j], &arr[j+1]);  
+} 
 
 
 
